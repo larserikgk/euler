9a19592f84e95203a01c5b6e893d732064f4931a
Lars Erik G-K <lars.erik.gresdal-knutrud@acando.no>
2017-02-09 12:47:57 +0100

    Improve solution for problem 79

--- problem_79.py
+++ problem_79.py
@@ -1,13 +1,12 @@
-
 class PasscodeAssembler:
     _passcode = []
     _derivations = []
 
-    def __init__(self, debug=False):
+    def __init__(self, debug=False, derivation_list=None):
         self._debug = debug
+        self._derivations = derivation_list
 
-    def add_derivation(self, input_derivation):
-        self._derivations.append(input_derivation)
+    def compute_passcode(self):
         for derivation in self._derivations:
             for index, number in enumerate(derivation):
                 if number not in self._passcode:
@@ -18,8 +17,8 @@ class PasscodeAssembler:
                             if not PasscodeAssembler.appears_before(reiterated_number, number, self._passcode) and\
                                     PasscodeAssembler.appears_before(reiterated_number, number, derivation):
                                 self.prepend_to(reiterated_number, number)
-        if self._debug:
-            print(str(input_derivation) + ' : ' + str(self._passcode))
+            if self._debug:
+                print(str(derivation) + ' : ' + str(self._passcode))
 
     def prepend_to(self, a, b):
         try:
@@ -28,25 +27,12 @@ class PasscodeAssembler:
         except ValueError:
             pass
 
-    def append_to(self, a, b):
-        try:
-            index_a, index_b = self._passcode.index(a), self._passcode.index(b)
-            self._passcode.insert(index_a+1, self._passcode.pop(index_b))
-        except ValueError:
-            pass
-
     @staticmethod
     def appears_before(arg1, arg2, list_arg):
         return list_arg.index(arg1) < list_arg.index(arg2)
 
-    @staticmethod
-    def elements_appear_in(list1, list2):
-        for element in list1:
-            if element in list2:
-                return True
-        return False
-
     def get_passcode(self):
+        self.compute_passcode()
         result = "Passcode: "
         for number in self._passcode:
             result += str(number)
@@ -55,8 +41,6 @@ class PasscodeAssembler:
 
 if __name__ == '__main__':
     f = open('problem_79_derivations.txt', 'r')
-    derivations = f.read().split()
-    assembler = PasscodeAssembler()
-    for derivation in derivations:
-        assembler.add_derivation(list(derivation))
+    derivation_lists = [list(item) for item in f.read().split()]
+    assembler = PasscodeAssembler(derivation_list=derivation_lists)
     print(assembler.get_passcode())
